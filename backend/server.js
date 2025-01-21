7371e34410b6f1b468b3774acb768fc70b36a8dd
Samuel <adeoyesamuel100@gmail.com>
2025-01-21 22:58:14 +0100

    update files

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// to be able tp parse the body of the request
+app.use(express.json());
+
+app.use(express.json()); // allow you to parse the body of the request
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+
+    connectDB();
+})
